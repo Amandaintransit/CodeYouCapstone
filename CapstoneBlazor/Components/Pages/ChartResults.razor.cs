19454a9c72bc2a92508bfdb2fed0266f77cdc05a
amanda-spears <amanda_spears@hotmail.com>
2024-12-07 09:30:31 -0500

    Interactivity

---
+++ CapstoneBlazor/Components/Pages/ChartResults.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace CapstoneBlazor.Components.Pages
+{
+    public partial class ChartResults
+    {
+        public decimal? AGIPrimary { get; set; }
+        public decimal AGIPrimarySecondary { get; set; }
+        public bool IsUnderSelfSupportReserveThreshold { get; set; } = false;
+        public decimal CombinedAGI { get; set; }
+
+        public decimal PercentageAGIPrimary { get; set; }
+        public decimal PercentageAGIPrimarySecondary { get; set; }
+        public decimal InitialObligation { get; set; }
+        public decimal TotalGuidelineAmount { get; set; }
+        public decimal PrimaryCustodianAllocation { get; set; }
+        public decimal SecondaryCustodianAllocation { get; set; }
+        public decimal FinalObligation { get; set; }
+    }
+}
--- CodeYouCapstone/Chart.cs
+++ CodeYouCapstone/Chart.cs
@@ -14,24 +14,15 @@ namespace CodeYouCapstone
         public decimal? MonthlyDeductionPriorChildrenPrimary { get; set; }
         public decimal? MonthlyDeductionPriorChildrenSecondary { get; set; }
         //AGI primary can only be 0 if caring for child under 3
-        public decimal? AGIPrimary { get; set; }    
+        
         public bool IsCaringForChildUnderThree { get; set; } = false;
        //cannot be 0 -minimum wage must be imputed
-        public decimal AGIPrimarySecondary { get; set; }
-        public bool IsUnderSelfSupportReserveThreshold { get; set; } = false;
-        public decimal CombinedAGI { get;  set; }
-
-        public decimal PercentageAGIPrimary { get; set; }   
-        public decimal PercentageAGIPrimarySecondary { get; set; }
-        public decimal InitialObligation { get; set; }
+     
         public decimal? ChildCarePrimary { get; set; }  
         public decimal? ChildCareSecondary { get; set; }
         public decimal? HealthInsurancePremiumPrimary { get; set; } 
         public decimal? HealthInsurancePremiumSecondary {  get; set; }
-        public decimal TotalGuidelineAmount {  get; set; }
-        public decimal PrimaryCustodianAllocation {  get; set; }
-        public decimal SecondaryCustodianAllocation { get; set; }   
-        public decimal FinalObligation { get; set; }
+        
 
         
     }
